57225f0d565f578d93d19edb356662fc29db8ac8
Sergio Barreras <sergiobg.isc@gmail.com>
2024-11-13 23:03:00 -0700

    Fixes #2: El proyecto ya ha sido configurado para trabajar con PostgreSQL

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Main');
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  await app.listen(process.env.PORT);
+  logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
